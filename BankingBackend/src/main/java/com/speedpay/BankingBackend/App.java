da901f946b3f89f88dcc0c1ee1880c08ef67343c
theara ouen <theara.ouen777@gmail.com>
2017-10-13 15:57:27 +0700

    My projeect set up complete successfully

---
+++ BankingBackend/src/main/java/com/speedpay/BankingBackend/App.java
@@ -0,0 +1,13 @@
+package com.speedpay.BankingBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
